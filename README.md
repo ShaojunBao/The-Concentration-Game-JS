5bf0d2e79cbfbb5e1510c26fabb08e65d8cf168c
Shaojun Bao <youngwolf111@gmail.com>
2023-07-09 18:12:32 -0400

    fixed issue when the user restart the game, but the timer still counts. Added some CSS to make the overall UI more user friendly

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # The-Concentration-Game-JS
+
+This is a memory game, players will click the card and try their best to memory the front image. Then try to find the similar image to match them. The moveCounter will start count player's movement once the player clicked two cards. The timer will start counting once the player click start.
+
+# Screenshot
+
+<img src="imags/screenshot01.png">
+<img src="imags/screenshot02.png">
+
+
+# Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+- CardStarter CSS Library
+
+# Next Steps
+
+- Future enhancement one...
+- Future enhancement two... 
+
+
--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // constants:
 const cardsValue = [
-    'imags/bacon.png',
-    'imags/bread.png',
-    'imags/egg.png',
-    'imags/hamburger.png',
-    'imags/noodles.png',
-    'imags/pizza.png',
-    'imags/ramen.png',
-    'imags/rice.png',
-]
+  'images/bacon.png',
+  'images/bread.png',
+  'images/egg.png',
+  'images/hamburger.png',
+  'images/noodles.png',
+  'images/pizza.png',
+  'images/ramen.png',
+  'images/rice.png',
+];
 
 // app's state (variables)
 let cards = [];
@@ -19,184 +19,178 @@ let time = 0;
 let moveCount = 0;
 let timerId = null;
 
-
 // cached element references:
 const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
 const timeDisplay = document.getElementById('timer');
 const moveCountDisplay = document.getElementById('move-counter');
+const deck = document.querySelector('.deck');
+const statusElement  = document.querySelector('.statusCheck');
 
-// event listeners:
-startBtn.addEventListener('click',startGame);
-stopBtn.addEventListener('click',stopGame);
+// event listener:
+startBtn.addEventListener('click', startGame);
+
 
 //Initialize the game
 startGame();
 
 // Function to create the game board
-function createBoard(cardsValueShuffled){
-    const deck = document.querySelector('.deck');
+function createBoard(cardsValueShuffled) {
+  // reset the current cards to prevent duplicate images
+  cards = [];
+  deck.innerHTML = '';
 
-    // reset the current cards to prevent duplicate images
-    cards = [];
-    deck.innerHTML = '';
+  // Generate the card deck
+  for (let i = 0; i < cardsValueShuffled.length; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.value = cardsValueShuffled[i];
 
-    //Generate the card deck 
-    for(let i=0; i<cardsValueShuffled.length; i++){
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.value = cardsValueShuffled[i];
+    const img = document.createElement('img');
+    img.src = 'images/face.png';
+    img.alt = 'Default image';
 
-        const img = document.createElement('img');
-        img.src = 'imags/face.png';
-        img.alt = 'Default image';
+    card.appendChild(img);
+    deck.appendChild(card);
+    cards.push(card);
+  }
 
-        card.appendChild(img);
-        deck.appendChild(card);
-        cards.push(card);
-    }
+  // Add OnClick event listener to every card.
+  cards.forEach((card) => {
+    card.addEventListener('click', function () {
+      // Start the timer only if it hasn't been started yet
+      if (!gameStarted) {
+        gameStarted = true;
+        startTimer();
+      }
+      flipCard(card);
+    });
+  });
 }
 
 // Function to shuffle the card values using Fisher-Yates algorithm
-function shuffle(arry){
-    let currentIdx = arry.length, randomIdx, temporary;
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex,
+    temporary;
 
-    while(--currentIdx >= 0){
-        randomIdx = Math.floor(Math.random()*(currentIdx+1));
-        temporary = arry[randomIdx];
-        
-        arry[randomIdx] = arry[currentIdx];
-        arry[currentIdx] = temporary;
-    }
-    return arry;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    temporary = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporary;
+  }
+
+  return array;
 }
 
-//Function to start the game
-function startGame(){
-    // Reset the game 
-    flippedCards = [];
-    matchedCards = [];
-    gameStarted = false;
-    moveCount = 0;
-    time = 0;
-    timeDisplay.textContent = "Time: 0s";
-    moveCountDisplay.textContent = "Moves: 0";
+// Function to start the game
+function startGame() {
+  stopTimer();
+  flippedCards = [];
+  matchedCards = [];
+  gameStarted = false;
+  moveCount = 0;
+  time = 0;
+  timeDisplay.textContent = 'Time: 0s';
+  moveCountDisplay.textContent = 'Moves: 0';
+  statusElement .textContent = '';
 
-    let cardsValuePickList = [...cardsValue,...cardsValue];
-    let shuffled = shuffle(cardsValuePickList);
+  let cardsValuePickList = [...cardsValue, ...cardsValue];
+  let shuffled = shuffle(cardsValuePickList);
 
-    createBoard(shuffled);
-
-    //Add OnClick event listener to every card.
-    for (let card of cards) {
-        card.addEventListener('click', function(){
-        flipCard(card);
-      });
-    }
+  createBoard(shuffled);
 }
 
-//Function to flip the card 
+// Function to flip the card
 function flipCard(card) {
-  //Start the time when the first card is fliped
-    if(!gameStarted){
-      gameStarted = true;
-      startTimer();
-    }
+  // Ensure the flipped card and matched cards cannot be re-flipped.
+  if (card.classList.contains('flipped') || card.classList.contains('matched')) {
+    return;
+  }
 
-    //Ensure the flipped card and matched cards cannot be refliped.
-    if (card.classList.contains('flipped') || card.classList.contains('matched')) {
-      return;
-    }
-    card.classList.add('flipped');
-    
+  card.classList.add('flipped');
 
-    const img = card.querySelector('img');
-    // Set the src to card's value
-    img.src = card.dataset.value;
-    
-    // Add the flipped card to the flippedCards array
-    flippedCards.push(card);
-  
-    if (flippedCards.length === 2) {
-      // Count to the Moves when flip 2 cards
-      moveCount++;
-      moveCountDisplay.textContent = `Moves: ${moveCount}`;
-      
-      disableCardFlipping(true);
-      
-      setTimeout(() =>{
-        // Two cards are flipped, check for a match
+  const img = card.querySelector('img');
+  // Set the src to card's value
+  img.src = card.dataset.value;
+
+  // Add the flipped card to the flippedCards array
+  flippedCards.push(card);
+
+  if (flippedCards.length === 2) {
+    // Count the moves when two cards are flipped
+    moveCount++;
+    moveCountDisplay.textContent = `Moves: ${moveCount}`;
+
+    disableCardFlipping(true);
+
+    setTimeout(() => {
+      // Two cards are flipped, check for a match
       checkForMatch();
-      //enable card flipping after delay
+      // Enable card flipping after delay
       disableCardFlipping(false);
-      },1000);
-      
-    }
-
+    }, 1000);
   }
+}
 
-  //Function to match the card
-  function checkForMatch() {
-    const card1 = flippedCards[0];
-    const card2 = flippedCards[1];
-  
-    const img1 = card1.querySelector('img');
-    const img2 = card2.querySelector('img');
-  
-    if (img1.src === img2.src) {
-      // The cards are a match
-      card1.classList.add('matched');
-      card2.classList.add('matched');
-      matchedCards.push(card1, card2);
-      
-    } else {
-      // The cards are not a match
-      setTimeout(() => {
-        card1.classList.remove('flipped');
-        card2.classList.remove('flipped');
-        //Change the img back to defalut img
-        img1.src = 'imags/face.png';
-        img2.src = 'imags/face.png';
-      }, 1000);
-    }
-  
-    // Clear the flippedCards array
-    flippedCards = [];
-  }
+// Function to match the cards
+function checkForMatch() {
+  const card1 = flippedCards[0];
+  const card2 = flippedCards[1];
 
-  //Function to start the timer
-  function startTimer(){
-    timerId = setInterval(() =>{
-      time++;
-      timeDisplay.textContent = `Time: ${time}s`;
+  const img1 = card1.querySelector('img');
+  const img2 = card2.querySelector('img');
+
+  if (img1.src === img2.src) {
+    // The cards are a match
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+    matchedCards.push(card1, card2);
+  } else {
+    // The cards are not a match
+    setTimeout(() => {
+      card1.classList.remove('flipped');
+      card2.classList.remove('flipped');
+      // Change the img back to default img
+      img1.src = 'images/face.png';
+      img2.src = 'images/face.png';
     }, 1000);
   }
 
-  //Function to reset the timer
-  function stopTimer(){
-    clearInterval(timerId);
-  }
+  // Clear the flippedCards array
+  flippedCards = [];
 
-//Function to stop the game
-function stopGame(){
-    flippedCards = [];
-    matchedCards = [];
+  //Check if all cards are matched
+  if(matchedCards.length === cards.length){
     stopTimer();
-    let cardsValuePickList = [...cardsValue,...cardsValue];
-    let shuffled = shuffle(cardsValuePickList);
-
-    createBoard(shuffled);
+    statusElement .textContent = 'Congratulations! You win!'
+  }
 }
 
-//Function to disable or enable card flipping
-function disableCardFlipping(disable){
-  for(let card of cards){
-    card.removeEventListener('click',flipCard);
-    if(disable){
+// Function to start the timer
+function startTimer() {
+  timerId = setInterval(() => {
+    time++;
+    timeDisplay.textContent = `Time: ${time}s`;
+  }, 1000);
+}
+
+// Function to reset the timer
+function stopTimer() {
+  clearInterval(timerId);
+}
+
+
+
+// Function to disable or enable card flipping
+function disableCardFlipping(disable) {
+  cards.forEach((card) => {
+    if (disable) {
       card.style.pointerEvents = 'none';
-    } else{
-      card.addEventListener('click',flipCard);
-      card.style.pointerEvents = 'auto'
+    } else {
+      card.style.pointerEvents = 'auto';
     }
-  }
+  });
 }
